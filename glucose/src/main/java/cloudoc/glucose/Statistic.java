f8a77e9a44c71ad14e7dba65c5a54b74569935c2
Pengjian <pengjian@cloudoc.cn>
2016-06-20 21:34:52 +0800

    修正可以运行

--- glucose/src/main/java/cloudoc/glucose/Statistic.java
+++ glucose/src/main/java/cloudoc/glucose/Statistic.java
@@ -48,8 +48,16 @@ public class Statistic implements java.io.Serializable
    @org.kie.api.definition.type.Label("????????2?????")
    private java.lang.Double twoHpgVariance = 0.0;
 
-   @org.kie.api.definition.type.Label(value = "??2????????")
+   @org.kie.api.definition.type.Label("??2????????")
    private java.lang.Integer twoHpgNormalCount = 0;
+
+   private java.lang.Boolean fbgCountUpdated = false;
+
+   private java.lang.Boolean fbgNormalCountUpdated = false;
+
+   private java.lang.Boolean twoHpgCountUpdated = false;;
+
+   private java.lang.Boolean twoHpgNormalCountUpdated = false;
 
    public Statistic()
    {
@@ -195,6 +203,47 @@ public class Statistic implements java.io.Serializable
       this.twoHpgNormalCount = twoHpgNormalCount;
    }
 
+   public java.lang.Boolean getFbgCountUpdated()
+   {
+      return this.fbgCountUpdated;
+   }
+
+   public void setFbgCountUpdated(java.lang.Boolean fbgCountUpdated)
+   {
+      this.fbgCountUpdated = fbgCountUpdated;
+   }
+
+   public java.lang.Boolean getFbgNormalCountUpdated()
+   {
+      return this.fbgNormalCountUpdated;
+   }
+
+   public void setFbgNormalCountUpdated(java.lang.Boolean fbgNormalCountUpdated)
+   {
+      this.fbgNormalCountUpdated = fbgNormalCountUpdated;
+   }
+
+   public java.lang.Boolean getTwoHpgCountUpdated()
+   {
+      return this.twoHpgCountUpdated;
+   }
+
+   public void setTwoHpgCountUpdated(java.lang.Boolean twoHpgCountUpdated)
+   {
+      this.twoHpgCountUpdated = twoHpgCountUpdated;
+   }
+
+   public java.lang.Boolean getTwoHpgNormalCountUpdated()
+   {
+      return this.twoHpgNormalCountUpdated;
+   }
+
+   public void setTwoHpgNormalCountUpdated(
+         java.lang.Boolean twoHpgNormalCountUpdated)
+   {
+      this.twoHpgNormalCountUpdated = twoHpgNormalCountUpdated;
+   }
+
    public Statistic(java.lang.Integer fbgCount, java.lang.Integer fbgNormalCount,
          java.lang.Double fbgNormalPercentage, java.lang.Integer twoHpgCount,
          java.lang.Double twoHpgNormalPercentage,
@@ -203,7 +252,11 @@ public class Statistic implements java.io.Serializable
          java.lang.Double fbgStandardDeviation, java.lang.Double fbgVariance,
          java.lang.Double averageTwoHpg,
          java.lang.Double twoHpgStandardDeviation,
-         java.lang.Double twoHpgVariance, java.lang.Integer twoHpgNormalCount)
+         java.lang.Double twoHpgVariance, java.lang.Integer twoHpgNormalCount,
+         java.lang.Boolean fbgCountUpdated,
+         java.lang.Boolean fbgNormalCountUpdated,
+         java.lang.Boolean twoHpgCountUpdated,
+         java.lang.Boolean twoHpgNormalCountUpdated)
    {
       this.fbgCount = fbgCount;
       this.fbgNormalCount = fbgNormalCount;
@@ -219,6 +272,10 @@ public class Statistic implements java.io.Serializable
       this.twoHpgStandardDeviation = twoHpgStandardDeviation;
       this.twoHpgVariance = twoHpgVariance;
       this.twoHpgNormalCount = twoHpgNormalCount;
+      this.fbgCountUpdated = fbgCountUpdated;
+      this.fbgNormalCountUpdated = fbgNormalCountUpdated;
+      this.twoHpgCountUpdated = twoHpgCountUpdated;
+      this.twoHpgNormalCountUpdated = twoHpgNormalCountUpdated;
    }
 
 }
